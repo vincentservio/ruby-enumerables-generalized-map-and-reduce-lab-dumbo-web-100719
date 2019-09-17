af2c0831eaa3f06c92f8d9ce5a41f42bf17e36e0
vincent servio <vincentservio@gmail.com>
2019-09-17 18:50:31 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length do 
+
+    
+    new.push(yield(array[i]))
+  i += 1
+end 
+return new 
+end 
+
+def reduce(array, v=nil)
+  if v 
+    total = v
+  i = 0 
+else 
+ total = array[0]
+ i = 1 
+ end 
+ 
+  while i < array.length 
+    total = yield(total, array [i])
+    
+  i+=1
+end
+total 
+end
